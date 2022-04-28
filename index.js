fb558824900b6872936d75ce9111fbbfa31c3de7
Wilson <double_j918@hotmail.com>
2022-04-28 22:49:00 +0800

    made some changes

--- index.js
+++ index.js
@@ -1,11 +1,72 @@
-const awaitPromise = async () => {
-    console.log("1. Before running Promise");
-  
-    try {
-      let promise = await new Promise((resolve, reject) => {
-        setTimeout(() => {
-          console.log("2. Inside SetTimeout");
-            // resolve("Hello");
-          reject("Bye bye");
-        }, 3000);
-      });
+
+
+let itemList = [];
+let taskaddcount = 0;
+let taskremovecount = 0;
+
+function createItem () {
+
+    let plusitem = document.getElementById('itemInput')
+    if (plusitem.value === ''){
+        alert('Please Add Task')
+    } else {
+    itemList.push(plusitem.value);
+    plusitem.value = '';
+    const newline = document.createElement('p');
+    const deletebtn = document.createElement('i');
+    const donebtn = document.createElement('i');
+
+    taskaddcount +=1;
+      for (let i = 0; i < itemList.length; i++){
+          document.body.append(newline);
+          newline.innerHTML = itemList[i];
+          newline.setAttribute('class', 'item');
+          document.getElementById('itemlist').appendChild(newline);
+
+          newline.appendChild(donebtn);
+          donebtn.setAttribute('id', 'donebtnid'+i);
+          donebtn.setAttribute('class', 'donebtn');
+          document.getElementById('donebtnid'+i).style.float = 'left';
+          donebtn.innerHTML = '<i class="fa-solid fa-circle-check" style = "font-size: 100%">';
+
+          newline.appendChild(deletebtn);
+          deletebtn.setAttribute('id', 'btnid'+i);
+          deletebtn.setAttribute('class', 'deletebtn');
+          document.getElementById('btnid'+i).style.float = 'right';
+          deletebtn.innerHTML = '<i class="fa-solid fa-trash-can" style = "font-size: 100%">';
+        }
+
+    document.getElementById(deletebtn.id).addEventListener('click', deleteItem);
+      function deleteItem() {
+          deletebtn.parentElement.remove();
+          if (deletebtn.parentElement.classList.contains('checked')){
+          } else {
+          taskremovecount += 1;
+          document.getElementById('taskno').innerHTML = taskaddcount- taskremovecount;                   
+          }
+        }
+
+    document.getElementById(donebtn.id).addEventListener('click', doneItem);
+        function doneItem() {
+          if (donebtn.parentElement.classList.contains('checked')){
+            donebtn.parentElement.classList.remove('checked');
+            taskaddcount +=1;
+            document.getElementById('taskno').innerHTML = taskaddcount- taskremovecount;
+          } else {
+          donebtn.parentElement.classList.add('checked');
+          taskremovecount += 1;
+          document.getElementById('taskno').innerHTML = taskaddcount- taskremovecount;                   
+          }
+        }
+
+    document.getElementById('clearbtn').addEventListener('click', clearItem);
+      function clearItem() {
+        newline.remove(); 
+        document.getElementById('taskno').innerHTML = 0;
+        taskaddcount = 0;
+        taskremovecount = 0;
+      }
+
+    document.getElementById('taskno').innerHTML = taskaddcount- taskremovecount;
+    }
+}
